552aebe508c63faffb2c9421811a0e39380174f1
GeomaticaAO <digitalizacionao@gmail.com>
2025-04-28 14:07:48 -0600

    mapabase.js

--- mapabase.js
+++ mapabase.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
             var div = L.DomUtil.create("div", "legend-container");
             div.innerHTML = `
                 <div class="legend">
-                    <img src="img/simbol/Simbologia.jpg" alt="Simbología" style="width: 150px; height: auto;">
+                    <img src="img/simbol/Simbologi.png" alt="Simbología" style="width: 150px; height: auto;">
                 </div>`;
             return div;
         },
